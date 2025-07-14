4be1f794e86cd05a6d6671bedd49d485380b4bbc
Brynna Tang <brynnatang@gmail.com>
2025-07-14 10:40:56 -0400

    Added list & @model and @query

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,17 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     @State private var showNewTask = false
     
+    
+    @Query var toDos: [ToDoItem]
     var body: some View {
         VStack {
             HStack{
                 Text("To-Do List")
                     .font(.system(size:40))
                     .fontWeight(.black)
+                
                 Spacer()
+                
+                
                 
                 Button{
                     withAnimation {
@@ -28,9 +34,16 @@ struct ContentView: View {
                         .fontWeight(.bold)
                 }
             }
+            
         }
         .padding()
         Spacer()
+        
+        List{
+            ForEach(toDos) { toDoItem in
+                Text(toDoItem.title)
+            }
+        }
         
         if showNewTask {
             NewToDoView()
